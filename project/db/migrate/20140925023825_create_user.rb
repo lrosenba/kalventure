b637fa6c4647009b4bed3a9e57bb4a45a1a9d5e4
aneyer <waterrose9@aol.com>
2014-09-26 18:15:01 -0700

    Update 20140925023825_create_user.rb

--- project/db/migrate/20140925023825_create_user.rb
+++ project/db/migrate/20140925023825_create_user.rb
@@ -3,6 +3,9 @@ class CreateUser < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :lastname
+      t.string :password
+      t.string :username
+      t.string :email
     end
   end
 end
